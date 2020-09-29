18b9d88bd45d59809d30981bd4b30dffa6995de6
Serena Zamarripa <serena@driftingcreatives.com>
2020-09-29 17:27:38 -0500

    Very basic homepage implementation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import './Home.css';
+import { Link } from "react-router-dom";
+
+
+// function Home() {
+//   return (<h2>Conservocean is an educational tool with the mission to bring awareness to how human avtivity impacts bodies of water and aquatic animals. </h2>);
+// }
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2 className="heading">Welcome!</h2>
+      <h2 className="subheading">Conservocean is an educational tool with the mission to bring awareness to how human activity impacts bodies of water and aquatic animals. </h2>
+      <ul className="list">
+        <li className="list-item">
+            <Link to="/bodies" className={"nav-link"}>
+                Bodies of Water
+            </Link>
+          
+        </li>
+        <li className="list-item">
+          Aquatic Animals
+          </li>
+        <li className="list-item">
+          Human Impacts
+          </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
