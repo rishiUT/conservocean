5aec995207c4ad51a950e55f5e913fc9a1d6d174
Rishi Salem <rishi.salem@outlook.com>
2020-09-30 02:16:20 +0000

    Added to About Page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -4,8 +4,15 @@ function About() {
   return (
     <div>
       <h2>About</h2>
+      <h2>total number of commits: </h2>
+      <h2>total number of issues: </h2>
+      <h2>Total number of unit tests: 0</h2>
     </div>
   );
 }
 
+//GET /projects/:id/repository/commits
+//sort through commits, get total number and number for each user id
+
+//gitlab access token = XyyzhHPKZLhojG-Z_3Qg
 export default About;
