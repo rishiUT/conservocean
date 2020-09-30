dc86c1e333eaa66b41cf9361130e6dbb29824026
Serena Zamarripa <serena@driftingcreatives.com>
2020-09-30 07:31:16 -0500

    Added navigation to models from splash page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import waves from './Media/waves.png';
 import fish from './Media/fish.png';
+import drops from './Media/water-drops.png';
 
 // function Home() {
 //   return (<h2>Conservocean is an educational tool with the mission to bring awareness to how human avtivity impacts bodies of water and aquatic animals. </h2>);
@@ -18,7 +19,7 @@ function Home() {
             <img className="image-1" src={fish} alt="fish"/>
           </div>
           <h2 className="heading">Welcome!</h2>
-          <h2 className="subheading">Conservocean is an educational tool with the mission to bring awareness to how human activity impacts bodies of water and aquatic animals. </h2>
+          <h2 className="subheading">ConservOcean is an educational tool with the mission to bring awareness to how human activity impacts bodies of water and aquatic animals. </h2>
         </div>
         <div className="img-cont">
           <img className="image" src={waves} alt="waves"/>
@@ -26,23 +27,35 @@ function Home() {
       </div>
 
       <div className="menu-section">
-        <ul className="list">
-          <li className="list-item">
-              <Link to="/water-bodies" className={"nav-link"}>
-                  Bodies of Water
-              </Link>
-          </li>
-          <li className="list-item">
-              <Link to="/species" className={"nav-link"}>
-                  Aquatic Animals
-              </Link>
-          </li>
-          <li className="list-item">
-              <Link to="/impacts" className={"nav-link"}>
-                  Human Impacts
-              </Link>
-          </li>
-      </ul>
+
+        <div className="text-content">
+          <div className="img-cont">
+            <img className="image-1" src={drops} alt="drops"/>
+          </div>
+          <h2 className="heading">Water covers over 70% of Earth's surface. </h2>
+          <h2 className="subheading">Explore Earth's oceans, marine life, and the impact of human factors</h2>
+
+        </div>
+
+        <div className="list-cont">
+          <ul className="list">
+            <li className="list-item">
+                <Link to="/water-bodies" className="button">
+                    Bodies of Water
+                </Link>
+            </li>
+            <li className="list-item">
+                <Link to="/species" className="button">
+                    Aquatic Animals
+                </Link>
+            </li>
+            <li className="list-item">
+                <Link to="/impacts" className="button">
+                    Human Impacts
+                </Link>
+            </li>
+          </ul>
+        </div>
     </div>
     </div>
   );
