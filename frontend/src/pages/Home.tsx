f3af276028adeacb784a57d941b35e2b29b90e40
Serena Zamarripa <serena@driftingcreatives.com>
2020-09-29 22:57:28 -0500

    Homepage hero block styled

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './Home.css';
 import { Link } from "react-router-dom";
+import waves from './Media/waves.png';
+import fish from './Media/fish.png';
 
 
 // function Home() {
@@ -10,22 +12,39 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h2 className="heading">Welcome!</h2>
-      <h2 className="subheading">Conservocean is an educational tool with the mission to bring awareness to how human activity impacts bodies of water and aquatic animals. </h2>
-      <ul className="list">
-        <li className="list-item">
-            <Link to="/bodies" className={"nav-link"}>
-                Bodies of Water
-            </Link>
-          
-        </li>
-        <li className="list-item">
-          Aquatic Animals
+
+      <div className="hero-section">
+        <div className="heading-content">
+          <div className="img-cont">
+            <img className="image-1" src={fish} alt="fish"/>
+          </div>
+          <h2 className="heading">Welcome!</h2>
+          <h2 className="subheading">Conservocean is an educational tool with the mission to bring awareness to how human activity impacts bodies of water and aquatic animals. </h2>
+        </div>
+        <div className="img-cont">
+          <img className="image" src={waves} alt="waves"/>
+        </div>
+      </div>
+
+      <div className="menu-section">
+        <ul className="list">
+          <li className="list-item">
+              <Link to="/water-bodies" className={"nav-link"}>
+                  Bodies of Water
+              </Link>
           </li>
-        <li className="list-item">
-          Human Impacts
+          <li className="list-item">
+              <Link to="/species" className={"nav-link"}>
+                  Aquatic Animals
+              </Link>
+          </li>
+          <li className="list-item">
+              <Link to="/impacts" className={"nav-link"}>
+                  Human Impacts
+              </Link>
           </li>
       </ul>
+    </div>
     </div>
   );
 }
