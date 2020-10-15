17cf97a0408a219a9f8df2ab22e8673e5f9f6f0e
Rishi Salem <rishi.salem@outlook.com>
2020-10-15 16:59:44 -0500

    Added outline for selenium unit tests

--- frontend/test/SeleniumTests.py
+++ frontend/test/SeleniumTests.py
@@ -10,6 +10,16 @@ driver = webdriver.Chrome(options=options, executable_path=PATH, )
 
 driver.get("https://conservocean.me")
 
+#Start at homepage
+#Ensure you can open about page
+    #Perhaps open it in a separate tab?
+    #Right now, that's all the model page does. If we implement anything else, like links to the tools we used, we can add to the tests.
+#Ensure you can open each model page
+    #On each model page, open several instances, just to make sure you can
+    #On each model page, use pagination to turn to the next page, then to turn to the last page, then to turn to page 2
+#That's all we have implemented, so close all the tabs and quit the browser
+
+
 print(driver.title)
 print(driver.current_url)
 #driver.forward()
