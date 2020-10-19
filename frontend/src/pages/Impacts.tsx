9def322b2e760144a02731f8389b96e9e9292899
Joe Wallery <joewallery@gmail.com>
2020-10-19 16:48:32 -0500

    Copy model page API logic to additional pages

--- frontend/src/pages/Impacts.tsx
+++ frontend/src/pages/Impacts.tsx
@@ -1,38 +1,34 @@
 import React, { Component } from "react";
 import ReactPaginate from "react-paginate";
-import {
-  Switch,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
+import { Switch, Route, Link, useParams } from "react-router-dom";
+import axios from "axios";
+
 import maine from "../assets/maine.png";
 import persianGulf from "../assets/persian-gulf.png";
 import taeanKorea from "../assets/taean-korea.png";
-import axios from "axios";
 
 import Map from "../parts/Map";
 
 interface impact {
-  name?: string
-  category?: string
-  subcategory?: string
-  description?: string
-  date?: string
-  latitude?: string
-  longitude?: string
-  oil_amount?: string
-  count_density_1?: string
-  count_density_2?: string
-  count_density_3?: string
-  count_density_4?: string
-  plant_rating?: string
-  plant_location?: string
-  plant_water_source?: string
+  name?: string;
+  category?: string;
+  subcategory?: string;
+  description?: string;
+  date?: string;
+  latitude?: string;
+  longitude?: string;
+  oil_amount?: string;
+  count_density_1?: string;
+  count_density_2?: string;
+  count_density_3?: string;
+  count_density_4?: string;
+  plant_rating?: string;
+  plant_location?: string;
+  plant_water_source?: string;
 
-  location?: string
-  capacity?: string
-  mapImgPath?: string
+  location?: string;
+  capacity?: string;
+  mapImgPath?: string;
 }
 
 const IMPACTS: impact[] = [
@@ -96,11 +92,12 @@ class Impacts extends Component {
     numInstances: 500,
   };
 
+  // Make API request for the current page of data using Axios
   loadData() {
-    // Axios API request
     // axios.get(`https://api.conservocean.me/api/impacts?offset={this.state.offset}&limit={this.state.perPage}`)
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(response => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => {
         console.log(response);
         this.setState({
           // Update the data and number of instances
@@ -108,11 +105,11 @@ class Impacts extends Component {
           // data: response.data.impacts,
           numInstances: this.state.numInstances,
           // numInstances: response.data.numInstances,
-        })
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-      })
+      });
   }
 
   // Component initially loads
@@ -122,13 +119,13 @@ class Impacts extends Component {
 
   handlePageClick = (data: any) => {
     console.log(data);
-    console.log(`Go to the selected page, page ${data.selected + 1}`)
+    console.log(`Go to the selected page, page ${data.selected + 1}`);
 
     // Change Offset: offset = (page number) x (# per page)
     this.setState({ offset: data.selected * this.state.perPage }, () => {
       this.loadData();
     });
-  }
+  };
 
   render() {
     return (
@@ -157,25 +154,25 @@ class Impacts extends Component {
                 {/* Pagination */}
                 <nav>
                   <ReactPaginate
-                    previousLabel={'previous'}
-                    nextLabel={'next'}
-                    breakLabel={'...'}
+                    previousLabel={"previous"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
                     pageCount={this.state.numInstances / this.state.perPage}
-                    marginPagesDisplayed={1} 
-                    pageRangeDisplayed={3} 
+                    marginPagesDisplayed={1}
+                    pageRangeDisplayed={3}
                     onPageChange={this.handlePageClick}
-                    containerClassName={'pagination'}
-                    breakClassName={'break-me'}
-                    breakLinkClassName={'page-link'}
-                    activeClassName={'active'}
-                    activeLinkClassName={'page-link'}
-                    pageClassName={'page-item'}
-                    pageLinkClassName={'page-link'}
-                    previousClassName={'page-item'}
-                    previousLinkClassName={'page-link'}
-                    nextClassName={'page-item'}
-                    nextLinkClassName={'page-link'}
-                    disabledClassName={'disabled'}
+                    containerClassName={"pagination"}
+                    breakClassName={"break-me"}
+                    breakLinkClassName={"page-link"}
+                    activeClassName={"active"}
+                    activeLinkClassName={"page-link"}
+                    pageClassName={"page-item"}
+                    pageLinkClassName={"page-link"}
+                    previousClassName={"page-item"}
+                    previousLinkClassName={"page-link"}
+                    nextClassName={"page-item"}
+                    nextLinkClassName={"page-link"}
+                    disabledClassName={"disabled"}
                   />
                 </nav>
               </div>
@@ -219,28 +216,63 @@ function Impact() {
         <main className="container py-5">
           <h1 className="text-center">{impact.name} </h1>
           <div className="container" style={{ width: "80%" }}>
-
-            <div style={{width: "100%", height: "500px"}}>
-              <Map lat={Number(impact.latitude)} lng={Number(impact.longitude)} zoom={4.75}/>
+            <div style={{ width: "100%", height: "500px" }}>
+              <Map
+                lat={Number(impact.latitude)}
+                lng={Number(impact.longitude)}
+                zoom={4.75}
+              />
             </div>
 
             <h3>Impact Details</h3>
             <ul>
               {impact.name ? <li>Name: {impact.name}</li> : null}
               {impact.category ? <li>Category: {impact.category}</li> : null}
-              {impact.subcategory ? <li>Subcategory: {impact.subcategory}</li> : null}
-              {impact.description ? <li>Description: {impact.description}</li> : null}
+              {impact.subcategory ? (
+                <li>Subcategory: {impact.subcategory}</li>
+              ) : null}
+              {impact.description ? (
+                <li>Description: {impact.description}</li>
+              ) : null}
               {impact.date ? <li>Date: {impact.date}</li> : null}
               {impact.latitude ? <li>Latitude: {impact.latitude}</li> : null}
               {impact.longitude ? <li>Longitude: {impact.longitude}</li> : null}
-              {impact.oil_amount ? <li>Oil Spilled: {impact.oil_amount} gallons</li> : null}
-              {impact.count_density_1 ? <li>Num 0.33-1.00mm pieces of plastic per km^2: {impact.count_density_1}</li> : null}
-              {impact.count_density_2 ? <li>Num 1.01-4.75mm pieces of plastic per km^2: {impact.count_density_2}</li> : null}
-              {impact.count_density_3 ? <li>Num 4.76-200mm pieces of plastic per km^2: {impact.count_density_3}</li> : null}
-              {impact.count_density_4 ? <li>Num 200+mm pieces of plastic per km^2: {impact.count_density_4}</li> : null}
-              {impact.plant_rating ? <li>Plant Rating: {impact.plant_rating}</li> : null}
-              {impact.plant_location ? <li>Plant Location: {impact.plant_location}</li> : null}
-              {impact.plant_water_source ? <li>Plant Water Source: {impact.plant_water_source}</li> : null}
+              {impact.oil_amount ? (
+                <li>Oil Spilled: {impact.oil_amount} gallons</li>
+              ) : null}
+              {impact.count_density_1 ? (
+                <li>
+                  Num 0.33-1.00mm pieces of plastic per km^2:{" "}
+                  {impact.count_density_1}
+                </li>
+              ) : null}
+              {impact.count_density_2 ? (
+                <li>
+                  Num 1.01-4.75mm pieces of plastic per km^2:{" "}
+                  {impact.count_density_2}
+                </li>
+              ) : null}
+              {impact.count_density_3 ? (
+                <li>
+                  Num 4.76-200mm pieces of plastic per km^2:{" "}
+                  {impact.count_density_3}
+                </li>
+              ) : null}
+              {impact.count_density_4 ? (
+                <li>
+                  Num 200+mm pieces of plastic per km^2:{" "}
+                  {impact.count_density_4}
+                </li>
+              ) : null}
+              {impact.plant_rating ? (
+                <li>Plant Rating: {impact.plant_rating}</li>
+              ) : null}
+              {impact.plant_location ? (
+                <li>Plant Location: {impact.plant_location}</li>
+              ) : null}
+              {impact.plant_water_source ? (
+                <li>Plant Water Source: {impact.plant_water_source}</li>
+              ) : null}
             </ul>
           </div>
         </main>
