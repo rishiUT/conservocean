59c97af15ca11afa93273184d34064eb76f3ad1d
Rishi Salem <rishi.salem@outlook.com>
2020-10-19 22:12:38 -0500

    Added file frontend.test.ts to write react tests

---
+++ frontend/test/frontend.test.ts
@@ -0,0 +1,10 @@
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Species from '../src/pages/Species';
+import Impacts from '../src/pages/Impacts';
+import WaterBodies from '../src/pages/WaterBodies';
+import About from '../src/pages/About';
+
+
