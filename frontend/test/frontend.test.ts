b4a5ca7ec1fa8daf0daae11f96f63fa4edeefd91
Rishi Salem <rishi.salem@outlook.com>
2020-10-19 22:28:13 -0500

    Fixed declaration in frontend tests

--- frontend/test/frontend.test.ts
+++ frontend/test/frontend.test.ts
@@ -1,10 +1,9 @@
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent } from '@testing-library/react';
 import Species from '../src/pages/Species';
 import Impacts from '../src/pages/Impacts';
 import WaterBodies from '../src/pages/WaterBodies';
-import About from '../src/pages/About';
-
+import About from '../src/pages/WaterBodies';
 
