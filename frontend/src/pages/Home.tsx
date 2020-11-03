0f7ddb1456a3223fa060b62def79fde0b6cafe3e
Serena Zamarripa <serena@driftingcreatives.com>
2020-11-02 20:57:03 -0600

    Minor styling changes for search

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,9 +5,30 @@ import waves from "./Media/waves.png";
 import fish from "./Media/fish.png";
 import drops from "./Media/water-drops.png";
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure, } from 'react-instantsearch-dom';
 
 const searchClient = algoliasearch('VEMEIF8QHL', 'e211f5541054cdb5177282492d4a90c8');
+
+function Hit(props: any) {
+  return (
+    <div>
+      <img src={props.hit.picture_url} alt={props.hit.name} />
+      <div className="hit-name">
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight attribute="description" hit={props.hit} />
+      </div>
+    </div>
+  );
+}
 
 
 function Home() {
@@ -67,9 +88,11 @@ function Home() {
       <div className="search-section">
         <h2 className="heading">Get started by searching.</h2>
         <InstantSearch searchClient={searchClient} indexName="conservocean">
+          <div className="right-panel">
             <SearchBox />
-            <Hits />
-          </InstantSearch>
+            <Hits hitComponent={Hit} />
+          </div>
+        </InstantSearch>
       </div>
     </div>
   );
