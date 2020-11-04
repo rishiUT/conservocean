9e741116e937a90c09b2fb282be2926eabeae578
AndyWeng33252 <andywengw8@gmail.com>
2020-11-04 00:45:22 +0000

    Update conservoceanAPI.py

--- backend/conservoceanAPI.py
+++ backend/conservoceanAPI.py
@@ -13,10 +13,10 @@ import re
 
 api = Api(app)
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def index(path):
-#     return render_template("index.html")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template("index.html")
 
 
 class FishList(Resource):
